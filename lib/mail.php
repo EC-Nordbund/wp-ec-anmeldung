31cb1866e761ff2e213528796388a95999268ce9
Tobias Krause <tobias_krause@akranet.de>
2018-11-03 22:46:26 +0100

    mail with placeholder and replacment

--- lib/mail.php
+++ lib/mail.php
@@ -5,6 +5,7 @@ function eca_confirmation_mail($to = '', $event_id = -1, $data = array(), $schem
     $error = $responce = array();
 
     $headers[] = 'From: EC-Nordbund <noreply@ec-nordbund.de>';
+    // $headers[] = 'Bcc: webmaster@ec-nordbund.de';
     $headers[] = 'Reply-To: Referent <referent@ec-nordbund.de>';
     $headers[] = 'Content-Type: text/html';
 
@@ -17,7 +18,9 @@ function eca_confirmation_mail($to = '', $event_id = -1, $data = array(), $schem
     }
 
     if(empty($error)) {
-        $message = eca_get_confirmation_mail_template();
+        $message = eca_get_confirmation_mail_template($data, $schema, $event);
+
+        // $message = json_encode($message);   // only for DEV purpose
 
         $mailed = wp_mail($to, 'Test', $message , $headers);
 
@@ -39,9 +42,12 @@ function eca_get_event_data($event_id) {
     if(function_exists('eme_get_event')) {
         $event = eme_get_event($event_id, false);
 
-        $event_out = array('title' => $eve);
+        $event_out = array(
+            'id' => $event['event_id'],
+            'title' => $event['event_name']
+        );
 
-        return $event;
+        return $event_out;
     }
 
     return false;
@@ -63,24 +69,38 @@ function eca_mail_to_wrapper($to, $data) {
     return $to;
 }
 
-function eca_get_confirmation_mail_template() {
+function eca_get_confirmation_mail_template($data, $schema, $event) {
     $template = file_get_contents( ECA_PLUGIN_DIR . '/lib/assets/confirmation_mail.html');
 
-    $matches = eca_replace_mail_shortcodes($template);
-
-    return $matches;
-}
-
-function eca_replace_mail_shortcodes($string) {
-    $regex = "/<<(.*?)>>/";
-    preg_match_all($regex, $string, $matches);
+    $regex = "/\*\|(.*?)\|\*/";
+    preg_match_all($regex, $template, $matches);
 
     for($i=0; $i < count($matches[1]); $i++) {
         $match = $matches[1][$i];
-
-        $replace = '@';
-        $string = str_replace($matches[0][$i], $replace, $string);
+        
+        $replace = eca_mail_get_replacements($match, $data, $schema, $event);
+        $template = str_replace($matches[0][$i], $replace, $template);
     }
 
-    return $string;
+    return $template;
+}
+
+function eca_mail_get_replacements($match, $data, $schema, $event) {
+    switch ($match) {
+        case 'anrede':
+            if(!empty($data['vorname'])) {
+                return 'Moin ' . $data['vorname'];
+            } else {
+                return 'Moin Moin,';
+            }
+            break;
+
+        case 'event_title':
+            return $event['title'];
+            break;
+        
+        default:
+            return '@';
+            break;
+    }
 }
